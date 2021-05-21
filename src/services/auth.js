3fb186c8436061e43010b6658e81059b609939d1
Andrew Cash <cashandrew41@gmail.com>
2021-05-21 06:52:23 -0500

    Add auth0

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,33 @@
+import auth0 from 'auth0-js'
+import { navigate } from "gatsby"
+
 export const isBrowser = () => typeof window !== "undefined"
+
+const auth = isBrowser 
+    ?   new auth0.WebAuth({
+            domain: process.env.AUTH0_DOMAIN,
+            clientID: process.env.AUTH0_CLIENTID,
+            redirectUri: process.env.AUTH0_CALLBACK,
+            responseType: "token id_token",
+            scope: "openid profile email"
+        })
+    : {}
+
+const tokens = {
+    accessToken: false,
+    idToken: false,
+    expiresAt: false,
+}
+
+let user = {}
+
+export const isAuthenticated = () => {
+    if (!isBrowser) {
+        return
+    }
+
+    return localStorage.getItem("isLoggedIn") === "true"
+}
 
 export const getUser = () =>
     isBrowser() && window.localStorage.getItem("gatsbyUser")
